2b5f0af6c779686fca44204d5f57b7bd16dbbf4c
mg218 <mgoss212@gmail.com>
2024-10-14 21:56:08 -0500

    Changed the CPUBurst variable to an array to handle different sizes

--- src/cpuscheduler/PCB.java
+++ src/cpuscheduler/PCB.java
@@ -5,13 +5,14 @@ public class PCB {
 	private String name;     // process name
 	private int id;          // process id
 	private int arrivalTime; // arrival time of the process
-	private int cpuBurst;    // CPU burst length in unit time
+	private int[] cpuBurst;    // CPU burst length in unit time
 	private int priority;    // priority level of the process
 	// the stats of the process execution
 	private int startTime, finishTime, turnaroundTime, waitingTime;
+	private int burstIndex;
 
 	// constructor
-	public PCB(String name, int id, int arrivalTime, int cpuBurst, int priority) {
+	public PCB(String name, int id, int arrivalTime, int[] cpuBurst, int priority) {
 		super();
 		this.name = name;
 		this.id = id;
@@ -20,6 +21,7 @@ public class PCB {
 		this.priority = priority;
 		this.startTime = -1;
 		this.finishTime = -1;
+		this.burstIndex =0;
 	}
 
 	public String getName() {
@@ -46,11 +48,11 @@ public class PCB {
 		this.arrivalTime = arrivalTime;
 	}
 
-	public int getCpuBurst() {
+	public int[] getCpuBurst() {
 		return cpuBurst;
 	}
 
-	public void setCpuBurst(int cpuBurst) {
+	public void setCpuBurst(int[] cpuBurst) {
 		this.cpuBurst = cpuBurst;
 	}
 
@@ -94,6 +96,12 @@ public class PCB {
 	public void setWaitingTime(int waitingTime) {
 		this.waitingTime = waitingTime;
 	}
+	public int getBurstIndex() {
+		return burstIndex;
+	}
+	public void setBurstIndex(int burstIndex) {
+		this.burstIndex=burstIndex;
+	}
 	public void increaseWaitingTime(int burst) {
 		//Increase the waitingTime variable with burst.
 		this.waitingTime+= burst;
@@ -101,8 +109,20 @@ public class PCB {
 	}
 	@Override
 	public String toString() {
-		return "PCB [name=" + name + ", id=" + id + ", arrivalTime=" + arrivalTime + ", cpuBurst=" + cpuBurst
-				+ ", priority=" + priority + "]";
+		String outp= "PCB [name=" + name + ", id=" + id + ", arrivalTime= " + arrivalTime + ", Priority= "+priority+", ";
+		//prints out all the bursts
+		for(int i=0;i<cpuBurst.length;i++) {
+			if(i%2==0) {
+				//if the process is an even index it is a cpu burst
+				outp+= "CPUburst= "+cpuBurst[i]+", ";
+			}else {
+				//if the process is an odd index it is an IO burst
+				outp+= "IO Burst= "+cpuBurst[i]+", ";
+			}
+		}
+		outp+="]";
+		
+		return outp;
 	}
 
 } 
