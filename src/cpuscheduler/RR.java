673dd06d3e5ee528fd805ba14fb9687d9ad41f95
mg218 <mgoss212@gmail.com>
2024-10-18 01:45:40 -0500

    Implemented ProcessEvent with SchedulingAlgorithm

--- src/cpuscheduler/RR.java
+++ src/cpuscheduler/RR.java
@@ -8,19 +8,22 @@ public class RR extends SchedulingAlgorithm {
 	}
 
 	public PCB pickNextProcess() {
+		// check to see if its the first process
 		if (systemTime == 0) {
 			return readyQueue.get(0);
 		} else {
+			// If the system time goes over an interval of the quantum time
 			if (systemTime % quantum == 0) {
-				if (procCount < readyQueue.size() - 1) {
-					procCount++;
+				if (procCount < readyQueue.size() - 1) {// if there is another process after the current one
+					procCount++;// go to the next one
 					return readyQueue.get(procCount);
-				} else {
-					procCount = 0;
+				} else {// if this the last process in the queue
+					procCount = 0;// reset to first process
 					return readyQueue.get(procCount);
 				}
 			} else {
-				while (procCount >= readyQueue.size()) {
+				while (procCount >= readyQueue.size()) {// if the process counter is larger than the size of the queue
+														// decrease the counter until its in bounds
 					procCount--;
 				}
 				return readyQueue.get(procCount);
--- src/cpuscheduler/SchedulingAlgorithm.java
+++ src/cpuscheduler/SchedulingAlgorithm.java
@@ -13,7 +13,10 @@ public abstract class SchedulingAlgorithm {
 	protected PCB curProcess; // current selected process by the scheduler
 	protected int systemTime; // system time or simulation time steps
 	protected int quantum;// Quantum Time for round robin
-	protected int procCount = 0;
+	protected int procCount = 0;// works as an index for process switching in round robin
+	protected int procCountLast=0;//keeps track of last executed process useful for RR
+	protected int ioCpuLast=0; //Keep track if the last executed process was IO or CPU Used for Event processing with round robin
+	
 
 	public SchedulingAlgorithm(String name, List<PCB> queue, int quantum) {
 		this.name = name;
@@ -43,44 +46,61 @@ public abstract class SchedulingAlgorithm {
 		for (PCB proc : allProcs) {
 			if (proc.getArrivalTime() <= systemTime) {
 				readyQueue.add(proc);
-				events.add(new ProcessEvent(proc,type.CREATED));
+				events.add(new ProcessEvent(proc, type.CREATED));
 			}
 		}
+		// remove the running processes from the all queue
 		allProcs.removeAll(readyQueue);
 		curProcess = pickNextProcess();
+		//checks to see if the queue and process changed and create a log if they do
+		if(name.equals("Round Robin")&&procCount!=procCountLast) {
+			if (curProcess.getBurstIndex() % 2 == 0&&ioCpuLast==1)
+				events.add(new ProcessEvent(curProcess,type.CPUQUEUE));
+			if(curProcess.getBurstIndex()%2==1&&ioCpuLast==0)
+				events.add(new ProcessEvent(curProcess,type.IOQUEUE));
+			
+		}
 		print();
 		if (curProcess.getStartTime() < 0)
 			curProcess.setStartTime(systemTime);
 
 		CPU.execute(curProcess, curProcess.getCpuBurst(), curProcess.getBurstIndex(), 1);
-		if(curProcess.getBurstIndex()%2==0)
-			events.add(new ProcessEvent(curProcess,type.CPU));
+		if (curProcess.getBurstIndex() % 2 == 0)
+			events.add(new ProcessEvent(curProcess, type.CPU));
 		else
-			events.add(new ProcessEvent(curProcess,type.IO));
+			events.add(new ProcessEvent(curProcess, type.IO));
 
 		for (PCB proc : readyQueue) {
 			if (proc != curProcess)
 				proc.increaseWaitingTime(1);
 
 		}
-
+		if (curProcess.getBurstIndex() % 2 == 0) 
+			ioCpuLast=0;
+		else 
+			ioCpuLast=1;
+		
 		systemTime++;
+		// finish up process
 		if (curProcess.getCpuBurst()[curProcess.getCpuBurst().length - 1] == 0) {
 			curProcess.setFinishTime(systemTime);
-			events.add( new ProcessEvent(curProcess,type.TERMINATED));
+			events.add(new ProcessEvent(curProcess, type.TERMINATED));
+			if (readyQueue.size() == 1)
+				events.add(new ProcessEvent(curProcess, type.DONE));
 			readyQueue.remove(curProcess);
 			finishedProcs.add(curProcess);
 			System.out.printf("Process %s terminated at %d start time = %d TAT =%d WT = %d", curProcess.getName(),
 					systemTime, curProcess.getStartTime(), curProcess.getTurnaroundTime(), curProcess.getWaitingTime());
 		}
+		// move the process to the next IO/CPU burst
 		if (curProcess.getCpuBurst()[curProcess.getBurstIndex()] == 0) {
 			curProcess.setBurstIndex(curProcess.getBurstIndex() + 1);
-			if(curProcess.getBurstIndex()%2==0)
-				new ProcessEvent(curProcess,type.CPUQUEUE);
+			if (curProcess.getBurstIndex() % 2 == 0)
+				events.add(new ProcessEvent(curProcess, type.CPUQUEUE));
 			else
-				new ProcessEvent(curProcess,type.IOQUEUE);
+				events.add(new ProcessEvent(curProcess, type.IOQUEUE));
 		}
-		
+		procCountLast=procCount;//Update the most recently executed process
 		System.out.println();
 		return events;
 	}
@@ -88,7 +108,7 @@ public abstract class SchedulingAlgorithm {
 	// print simulation step
 	public void print() {
 		// add code to complete the method
-		if(curProcess.getBurstIndex()%2==0)
+		if (curProcess.getBurstIndex() % 2 == 0)
 			System.out.printf("CPU %s\n", curProcess == null ? "Idle" : curProcess.getName());
 		else
 			System.out.printf("IO %s\n", curProcess == null ? "Idle" : curProcess.getName());
